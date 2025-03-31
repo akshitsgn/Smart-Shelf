68043f4f4658d28a880a7d32191a96dc5c94e591
https://github.com/akshitsingh04/language_converter.git <unknown.1104.horror@gmail.com>
2025-03-31 23:13:01 +0530

    The basic structure of the app

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("org.jetbrains.kotlin.kapt") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.51" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin) apply false
+}
