f1e8b1f9ee55486c007a9bd018129220d2a138f8
akshitsgn <akshit11a.21774@gmail.com>
2025-04-01 11:36:54 +0530

    Added the book dto , repository in domain and data layer

--- app/src/main/java/com/example/bookapp/data/dto/BookDto.kt
+++ app/src/main/java/com/example/bookapp/data/dto/BookDto.kt
@@ -1,2 +1,10 @@
 package com.example.bookapp.data.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BookDto(
+    val title: String,
+    val genre: String,
+    val imageUrl: String
+)
--- app/src/main/java/com/example/bookapp/domain/repository/BookRepository.kt
+++ app/src/main/java/com/example/bookapp/domain/repository/BookRepository.kt
@@ -1,2 +1,8 @@
 package com.example.bookapp.domain.repository
 
+import com.example.bookapp.domain.model.Book
+import com.example.bookapp.utils.ApiResponse
+
+interface BookRepository {
+    suspend fun getRecommendations(userBooks: List<Book>): ApiResponse<List<Book>>
+}
