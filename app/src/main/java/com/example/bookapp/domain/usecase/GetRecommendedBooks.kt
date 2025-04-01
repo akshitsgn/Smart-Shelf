a42a4d3bfe6e4bf0fbc4751151768c067f4c4a28
akshitsgn <akshit11a.21774@gmail.com>
2025-04-01 12:06:02 +0530

    Added the book usecase

--- app/src/main/java/com/example/bookapp/domain/usecase/GetRecommendedBooks.kt
+++ app/src/main/java/com/example/bookapp/domain/usecase/GetRecommendedBooks.kt
@@ -1,2 +1,12 @@
 package com.example.bookapp.domain.usecase
 
+import com.example.bookapp.domain.model.Book
+import com.example.bookapp.domain.repository.BookRepository
+import com.example.bookapp.utils.ApiResponse
+
+
+class GetRecommendedBooksUseCase(private val repository: BookRepository) {
+    suspend operator fun invoke(userBooks: List<Book>): ApiResponse<List<Book>> {
+        return repository.getRecommendations(userBooks)
+    }
+}
